779565b51673b654fe299c5c698a6b2d86585a02
ybchar <uiurihappy@sju.ac.kr>
2022-12-11 01:58:17 +0900

    feat: Mixin

--- inheritance.main.dart
+++ inheritance.main.dart
@@ -8,27 +8,54 @@ class Human {
   }
 }
 
-enum Team { red : {'redTeam'}, blue, pupple }
+enum Team { red, blue, pupple }
 
 // Inheritance
-class Player extends Human {
+class Player with Strong, QuickRunner, Tall {
   final Team team;
 
-// name이란 변수를 super 클래스, 즉 부모 클래스에게 전달
   Player({
     required this.team,
-    required String name,
-  }) : super(name: name);
+  });
 
-  @override
-  void sayHello() {
-    super.sayHello();
-    print("and I play for ${team}");
+// name이란 변수를 super 클래스, 즉 부모 클래스에게 전달
+  // Player({
+  //   required this.team,
+  //   required String name,
+  // }) : super(name: name);
+
+  // @override
+  // void sayHello() {
+  //   super.sayHello();
+  //   print("and I play for ${team}");
+  // }
+}
+
+// Mixin: 생성자 없는 클래스
+// 하나의 클래스에서 단 한번만 사용할 거면 의미가 없음, 차라리 상속을 씀
+// 장점: 여러 클래스에서 재사용성이 뛰어남
+class Strong {
+  final double strengthLevel = 100.00;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print("runnnnnn!");
   }
 }
 
-void main(List<String> args) {
-  var player = Player(team: Team.red, name: 'ybchar');
+class Tall {
+  final double height = 1.9;
+}
 
-  player.sayHello();
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
+void main(List<String> args) {
+  // var player = Player(team: Team.red, name: 'ybchar');
+
+  // player.sayHello();
+
+  var player = Player(team: Team.red,)
 }
